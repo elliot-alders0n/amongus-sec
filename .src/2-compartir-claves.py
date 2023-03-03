fd7c8d093a674ce3d64eddebd1f3c45a40955236
elliot-alders0n <121522020+elliot-alders0n@users.noreply.github.com>
2023-03-03 16:36:12 +0100

    Update 2-compartir-claves.py

--- .src/2-compartir-claves.py
+++ .src/2-compartir-claves.py
@@ -8,12 +8,14 @@ with open('conf/amongus-sec.conf') as f:
 	lines = f.readlines()
 	datos = []
 	for line in lines:
-		ip,nombre = line.rstrip().split('\t')
+		line = line.strip()
+		if line[0] != '#':
+			ip,nombre = line.rstrip().split('\t')
 
-		if ip != mi_ip:
-			datos.append([ip,nombre])
-		else:
-			mi_nombre = nombre
+			if ip != mi_ip:
+				datos.append([ip,nombre])
+			else:
+				mi_nombre = nombre
 
 for dato in datos:
 	comando = f"ssh-copy-id -i .claves/{mi_nombre}.pub kali@{dato[0]}"
